c2ab17bd3ac4297f806037bb4ca1c5d8d52f2c0a
asthakumari021196 <astha.kumari0211@gmail.com>
2021-02-08 03:33:29 +0530

    changes

--- consumer.py
+++ consumer.py
@@ -17,31 +17,27 @@ movies = []
 client = KafkaClient(hosts='localhost:9092')
 topic = client.topics['movies']
 
-try:
-    consumer = topic.get_simple_consumer(
-        consumer_group='test-consumer-group', 
-        auto_offset_reset=OffsetType.LATEST)
+
+
+for msg in client.topics['movies'].get_simple_consumer():
+    data = '{0}'.format(msg.value.decode())
     # Process should be listening all the time to receive new messages
-    while True:
-        for i in consumer:
-            if i is not None:
-                data = '{0}'.format(i.value.decode())
-                # get data only in json format and ignore other data
-                try:
-                    movie = json.loads(data)
-                    print('===== ',movie['original_title'])
-                    if len(list(collection.find({'_id':movie['_id']}))) == 0:
-                        collection.insert_one(movie)
-                        print('Movie validated and inserted')
-                    else:
-                        print('Movie validated and not inserted')
-                except ValueError:
+    if msg is not None:
+        data = '{0}'.format(msg.value.decode())
+        # get data only in json format and ignore other data
+        try:
+            movie = json.loads(data)
+            print('===== ',movie['original_title'])
+            if len(list(collection.find({'_id':movie['_id']}))) == 0:
+                collection.insert_one(movie)
+                print('Movie validated and inserted')
+            else:
+                print('Movie validated and not inserted')
+        except ValueError:
                     print('Another message that is not a movie')
                     print(data)
                     continue
-            else:
+        else:
                 continue
-        
-except KeyboardInterrupt:
-    print('Process stopped')
+
     
